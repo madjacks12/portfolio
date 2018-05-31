d061f559fea674d1e6ded74cdd9e92ea8b3081c3
MJ <mdjacksonvideo@gmail.com>
2018-05-30 21:52:30 -0700

    add background image

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -16,31 +16,32 @@ export default class  Home extends Component {
     return (
       <div>
         <h1>Matt Jackson</h1>
-        <h2>Full-Stack Developer</h2>
 
-        <Typing loop speed={80}>
-          <h3>Java
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={4} speed={80}  />
-          <Typing.Delay ms={500} />
-          React
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={5} speed={80}  />
-          <Typing.Delay ms={500} />
-          React-Native
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={12} speed={80}  />
-          <Typing.Delay ms={500} />
-          Android
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={7} speed={80}  />
-          <Typing.Delay ms={500} />
-          Node.js
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={7} speed={80}  />
-          <Typing.Delay ms={1000} />
-          </h3>
-        </Typing>
+
+          <Typing loop speed={80}>
+            <h3>Java
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={4} speed={80}  />
+            <Typing.Delay ms={500} />
+            React
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={5} speed={80}  />
+            <Typing.Delay ms={500} />
+            React-Native
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={12} speed={80}  />
+            <Typing.Delay ms={500} />
+            Android
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={7} speed={80}  />
+            <Typing.Delay ms={500} />
+            Node.js
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={7} speed={80}  />
+            <Typing.Delay ms={1000} />
+            </h3>
+          </Typing>
+        <h2>Full-Stack Developer</h2>
       </div>
 
     );
