4b6f8795a58e321ccef4da5bd30c9aae5a4a2a53
Shannon Alexander <shannonalexander@MacBook-Air.local>
2018-06-04 20:05:53 -0700

    add underlines under titles

--- src/containers/About.js
+++ src/containers/About.js
@@ -16,6 +16,9 @@ export default class Home extends Component {
     return (
       <div>
         <h1 id="about">About Me</h1>
+          <div id="stroke">
+            <h1 id="aboutLine">I</h1>
+          </div>
           <div id="container">
             <div id="columns">
               <p>Im an AWS certified developer and Cloud Solutions Architect specializing in mobile development with Android and React Native. Im just as comfortable building engaging front-end UIs and animations as I am building a scalable back-end. I recently graduated from one of the top coding bootcamps in Portland, OR and am seeking new opportunities in the Indianapolis, IN area.
